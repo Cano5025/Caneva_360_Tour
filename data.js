fc7d20e906a2072581402c629d8273a5f026f5ae
Cano5025 <lucas_canosa@outlook.com>
2026-03-20 12:41:11 -0300

    Caneva v2 - Tour Completo

---
+++ data.js
@@ -0,0 +1,577 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-01_entrada",
+      "name": "01_Entrada",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 0.25686498074418296,
+        "pitch": 0.18396242894099046,
+        "fov": 1.4850478238092877
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.40113548510859687,
+          "pitch": 0.7107563173162639,
+          "rotation": 0,
+          "target": "1-02_living_room"
+        },
+        {
+          "yaw": 2.6228133783974714,
+          "pitch": 0.32978824399756235,
+          "rotation": 0,
+          "target": "12-13_kitchen"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-02_living_room",
+      "name": "02_Living_Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -1.84933769984689,
+        "pitch": 0.21387748374669968,
+        "fov": 1.4850478238092877
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.643097862701623,
+          "pitch": 0.7636154533859347,
+          "rotation": 0.7853981633974483,
+          "target": "2-03_living_room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-03_living_room",
+      "name": "03_Living_Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -2.903840929936159,
+        "pitch": 0.10680339840879327,
+        "fov": 1.4850478238092877
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9182810340010477,
+          "pitch": 0.5971907041995799,
+          "rotation": 18.84955592153877,
+          "target": "3-04_living_room1"
+        },
+        {
+          "yaw": -2.30814874908385,
+          "pitch": 0.5033886037945301,
+          "rotation": 0.7853981633974483,
+          "target": "4-04_living_room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-04_living_room1",
+      "name": "04_Living_Room1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 3.122548415307845,
+        "pitch": 0.13936035385484757,
+        "fov": 1.4850478238092877
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.7342100968888854,
+          "pitch": 0.5928530267884202,
+          "rotation": 0,
+          "target": "5-05_tv"
+        },
+        {
+          "yaw": -1.8243371190700977,
+          "pitch": 0.5597599369313357,
+          "rotation": 5.497787143782138,
+          "target": "4-04_living_room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-04_living_room",
+      "name": "04_Living_Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 2.9852558139045167,
+        "pitch": 0.06893766850926397,
+        "fov": 1.4850478238092877
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.38880661778256,
+          "pitch": 0.519990515055337,
+          "rotation": 0.7853981633974483,
+          "target": "6-07_comedor"
+        },
+        {
+          "yaw": -2.7269915483168354,
+          "pitch": 0.31779941392857936,
+          "rotation": 0,
+          "target": "7-08_comedor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-05_tv",
+      "name": "05_Tv",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 1.46820302948632,
+        "pitch": 0.11261384885441217,
+        "fov": 1.4850478238092877
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1084436639655344,
+          "pitch": 0.41665380833749843,
+          "rotation": 0,
+          "target": "6-07_comedor"
+        },
+        {
+          "yaw": -1.94036523146546,
+          "pitch": 0.6222417350546117,
+          "rotation": 0,
+          "target": "3-04_living_room1"
+        },
+        {
+          "yaw": -2.816320920158205,
+          "pitch": 0.11739159458466375,
+          "rotation": 0,
+          "target": "7-08_comedor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-07_comedor",
+      "name": "07_Comedor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -0.3575319997555546,
+        "pitch": 0.10458083266261298,
+        "fov": 1.4850478238092877
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.000264339308032,
+          "pitch": 0.13949029955402992,
+          "rotation": 4.71238898038469,
+          "target": "7-08_comedor"
+        },
+        {
+          "yaw": 0.7721996684975849,
+          "pitch": 0.5325932612347586,
+          "rotation": 0,
+          "target": "4-04_living_room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-08_comedor",
+      "name": "08_Comedor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -0.07391759173778212,
+        "pitch": 0.15225738872940653,
+        "fov": 1.4850478238092877
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.17362720739246917,
+          "pitch": 0.49796468718808207,
+          "rotation": 0,
+          "target": "8-09_pasillo"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-09_pasillo",
+      "name": "09_Pasillo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 2.648932371324692,
+        "pitch": 0.3543420745329815,
+        "fov": 1.4850478238092877
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.25395349513809684,
+          "pitch": 0.6744155467828836,
+          "rotation": 0,
+          "target": "7-08_comedor"
+        },
+        {
+          "yaw": 2.188224310240366,
+          "pitch": 0.8351082707519524,
+          "rotation": 6.283185307179586,
+          "target": "9-10_office"
+        },
+        {
+          "yaw": -1.6644773660230587,
+          "pitch": 0.6937466779272601,
+          "rotation": 0.7853981633974483,
+          "target": "12-13_kitchen"
+        },
+        {
+          "yaw": -2.4720785837019363,
+          "pitch": 0.5549269324484527,
+          "rotation": 5.497787143782138,
+          "target": "11-12_laundry_room"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-10_office",
+      "name": "10_Office",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 1.3769487620722494,
+        "pitch": 0.13857077665524642,
+        "fov": 1.4850478238092877
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.488927490706807,
+          "pitch": 0.7876043604904144,
+          "rotation": 0,
+          "target": "8-09_pasillo"
+        },
+        {
+          "yaw": -1.8127367401777619,
+          "pitch": 0.30405035528603896,
+          "rotation": 0,
+          "target": "12-13_kitchen"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-11_pasillo",
+      "name": "11_Pasillo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-12_laundry_room",
+      "name": "12_Laundry_Room",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 0.8459476252488933,
+        "pitch": 0.12025720617549496,
+        "fov": 1.4850478238092877
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.2461271753059755,
+          "pitch": 0.545234867964755,
+          "rotation": 0,
+          "target": "10-11_pasillo"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.08788245789907734,
+          "pitch": 0,
+          "title": "Lavadero",
+          "text": "- 3 Lavarropas<div>- 2&nbsp; Espacios Lavarropas&nbsp;</div><div>y secarropas</div>"
+        }
+      ]
+    },
+    {
+      "id": "12-13_kitchen",
+      "name": "13_Kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.14453067993581037,
+          "pitch": 0.6629408337894382,
+          "rotation": 0,
+          "target": "10-11_pasillo"
+        },
+        {
+          "yaw": -1.4780244526740756,
+          "pitch": 0.28803498072953104,
+          "rotation": 0,
+          "target": "0-01_entrada"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Casa Roldan",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
